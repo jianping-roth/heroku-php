2ec7715b72215aec174096f877674b8d0f100f00
jianping roth <jianping.roth@gmail.com>
2013-02-20 22:33:58 -0800

    changed

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+require 'php-sdk/facebook.php';
+
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+  'appId'  => '457681004272032',
+  'secret' => '3e3d797f61d5d598ff6a07782c47a6c5',
+));
+$user = $facebook->getUser();
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+
 <HTML>
 <HEAD>
 <TITLE> Hello World in PHP </TITLE>
@@ -6,6 +24,7 @@
 <?
 // Hello world in PHP
  print("Hello World");
+ echo "User ID: ', $user';
  ?>
  </BODY>
  </HTML>
