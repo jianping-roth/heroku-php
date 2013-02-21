8e7d133accb4e9b279ce95ce578d30230b4e309d
jianping roth <jianping.roth@gmail.com>
2013-02-20 22:46:38 -0800

    changed

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?
-require 'php-sdk/facebook.php';
-
-// Create our Application instance (replace this with your appId and secret).
-$facebook = new Facebook(array(
-  'appId'  => '457681004272032',
-  'secret' => '3e3d797f61d5d598ff6a07782c47a6c5',
-));
-$user = $facebook->getUser();
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    error_log($e);
-    $user = null;
-  }
-}
+	require 'php-sdk/facebook.php';
+	
+	// Create our Application instance (replace this with your appId and secret).
+	$facebook = new Facebook(array(
+	  'appId'  => '457681004272032',
+	  'secret' => '3e3d797f61d5d598ff6a07782c47a6c5',
+	));
+	$user = $facebook->getUser();
+	if ($user) {
+	  try {
+	    // Proceed knowing you have a logged in user who's authenticated.
+	    $user_profile = $facebook->api('/me');
+	  } catch (FacebookApiException $e) {
+	    error_log($e);
+	    $user = null;
+	  }
+	}
 ?>
 <HTML>
 <HEAD>
-<TITLE> Hello World in PHP </TITLE>
+<TITLE>Facebook PHP </TITLE>
 </HEAD>
 <BODY>
+<h1>Hello World</h1>
 <?
-// Hello world in PHP
- print("Hello World");
- echo 'User ID: ', $user, '</p>';
+	if ($user) :
+ 		echo '<p>User ID: ', $user, '</p>';
+    else: 
+        $loginUrl = $facebook->getLoginUrl();
+        echo '<p><a href="', $loginUrl, '" target="_top">login</a></p>';
+    endif;
  ?>
  </BODY>
  </HTML>
