856429d6f9bc8fd26703d2c498b9112e03d6cc10
jianping roth <jianping.roth@gmail.com>
2013-02-22 23:40:34 -0800

    changed

--- index.php
+++ index.php
@@ -28,10 +28,11 @@
 	if ($user) :
  		echo '<p class="notes"><a href="logout.php">logout</p>';
  		$user_graph = $facebook->api('/me/friends','GET');
-
+        $moviePath='me/friends?fields=movies.fields(likes,id,name,created_time,picture.width(100).height(100).type(square),link,description)';
+        $movie_graph = $facebook->api($moviePath);
 
         echo '<div class="movegroupp">';
-        foreach ($user_graph['data'] as $key => $value) {
+        foreach ($movie_graph['data'] as $key => $value) {
             echo '<div class="friendinfo group">';
             echo '<a href="http://facebook.com/', $value['id'], '" target="_top">';
             echo '<img class="frienddumb" src="https://graph.facebook.com/',
