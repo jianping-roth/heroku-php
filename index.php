4038930dc7102f8d319fd101636fc03eb99db5b0
jianping roth <jianping.roth@gmail.com>
2013-02-23 21:32:56 -0800

    changed

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	));
 	$user = $facebook->getUser();
     $qty = 5;
-    $currentPage = 1;
     $currOffset = $_GET('offset');
 ?>
 <HTML>
@@ -21,8 +20,7 @@
 <?
 	if ($user) :
  		echo '<p class="notes"><a href="logout.php">logout</p>';
-        $moviePath='me/friends?fields=id,name,
-        movies.fields(likes,id,name,created_time,picture.width(100).height(100).type(square),link,description)
+        $moviePath='me/friends?fields=id,name,movies.fields(likes,id,name,created_time,picture.width(100).height(100).type(square),link,description)
         &limit='.$qty.'&offset='.$currOffset;
         $movies_graph = $facebook->api($moviePath);
 
